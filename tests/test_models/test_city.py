0df996277926e3a95276ce6f770613bfd2bafd11
Khairat Adesina <khairatadesina01@gmail.com>
2023-11-18 22:24:18 +0100

    fix: fixed all pycodestyle and documentation

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-""" """
+"""Test ciy module """
 from tests.test_models.test_base_model import test_basemodel
 from models.city import City
 
 
 class test_City(test_basemodel):
-    """ """
+    """ Class that tests for city"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """Constructor"""
         super().__init__(*args, **kwargs)
         self.name = "City"
         self.value = City
 
     def test_state_id(self):
-        """ """
+        """Testing state id"""
         new = self.value()
         self.assertEqual(type(new.state_id), str)
 
     def test_name(self):
-        """ """
+        """Testing name"""
         new = self.value()
         self.assertEqual(type(new.name), str)
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,34 +1,34 @@
 #!/usr/bin/python3
-""" """
+""" The user moduke"""
 from tests.test_models.test_base_model import test_basemodel
 from models.user import User
 
 
 class test_User(test_basemodel):
-    """ """
+    """ Th class test user """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ constructor"""
         super().__init__(*args, **kwargs)
         self.name = "User"
         self.value = User
 
     def test_first_name(self):
-        """ """
+        """ Teat first name"""
         new = self.value()
         self.assertEqual(type(new.first_name), str)
 
     def test_last_name(self):
-        """ """
+        """ Test last name"""
         new = self.value()
         self.assertEqual(type(new.last_name), str)
 
     def test_email(self):
-        """ """
+        """ Test email"""
         new = self.value()
         self.assertEqual(type(new.email), str)
 
     def test_password(self):
-        """ """
+        """ Test password"""
         new = self.value()
         self.assertEqual(type(new.password), str)
