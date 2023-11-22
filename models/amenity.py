bd9553620c3eff54534e1e5103e58ecb461aa247
Josephdada01 <ridwandada500@gmail.com>
2023-11-22 12:43:05 +0100

    Committing changes before pull

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
 
 
 class Amenity(BaseModel):
+    """"""
     name = ""
