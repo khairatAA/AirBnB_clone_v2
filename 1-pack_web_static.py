2fbd359e65c77ee6f7732dbaded34fb814e2f4c2
Khairat Adesina <khairatadesina01@gmail.com>
2023-12-07 15:05:01 -0800

    fix: run fab with the file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,19 +11,23 @@ def do_pack():
     your AirBnB Clone repo
     """
 
+    """Check if version dir exits:"""
     path = "versions"
     if not os.path.exists(path):
         os.makedirs(path)
 
+    """Get the current time and date"""
     currentDateAndTime = datetime.now()
     archiveTime = currentDateAndTime.strftime("%Y%m%d%H%M%S")
 
+    """Naming of the archive file"""
     name_of_archive = "web_static_{}.tgz".format(archiveTime)
 
     archive_path = "{}/{}".format(path, name_of_archive)
 
     content = "web_static"
 
+    """Using of the tar command"""
     result = local("tar -cvzf {} {}".format(archive_path, content))
 
     if result.succeeded:
