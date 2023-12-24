9839e831f7f554e61b1587b4635ad0f98d4efd1b
Khairat Adesina <khairatadesina01@gmail.com>
2023-12-24 22:18:17 +0100

    add: Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,16 @@
-from flask import Flask
+#!/usr/bin/python3
 """0-hello_route module"""
+from flask import Flask
 
 
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def display():
     """Starts a Flask web application"""
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
