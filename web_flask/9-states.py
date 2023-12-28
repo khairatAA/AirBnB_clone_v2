034980afd4788e05b4506c664f1c9bb174078c23
Khairat Adesina <khairatadesina01@gmail.com>
2023-12-28 22:51:00 +0100

    add: routes to /state and /state/<id>

---
+++ web_flask/9-states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""7-states_list module"""
+from flask import Flask, render_template
+from models import storage
+from models import *
+
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<state_id>', strict_slashes=False)
+def states(state_id=None):
+    """states: display a HTML with the state name and id"""
+    state_obj = storage.all(State)
+    city_obj = storage.all(City)
+
+    return render_template(
+            '9-states.html',
+            state_obj=state_obj,
+            state_id=state_id,
+            city_obj=city_obj
+            )
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """To remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
